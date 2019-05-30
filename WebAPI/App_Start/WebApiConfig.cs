d23688c37de328260078b116020fab38824459de
Rezz <rezzpad@gmail.com>
2019-05-30 09:41:44 -0400

    Complete

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPI
 {
     public static class WebApiConfig
     {
+         
         public static void Register(HttpConfiguration config)
         {
+            //Enable CORS
+            var corsAttr = new EnableCorsAttribute("http://localhost:4200", "*",  "*");
+            config.EnableCors(corsAttr);
+
             // Web API configuration and services
 
             // Web API routes
